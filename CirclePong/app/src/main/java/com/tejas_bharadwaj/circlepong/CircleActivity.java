671bc1f86409d84f6b2376f77c91fb827a25e486
tejaskb04 <tejaskb@gmail.com>
2017-09-17 15:08:08 -0700

    Canvas and Buttons Displayed

--- CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/CircleActivity.java
+++ CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/CircleActivity.java
@@ -1,23 +1,13 @@
 package com.tejas_bharadwaj.circlepong;
 
-import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class CircleActivity extends AppCompatActivity {
-
-    private Button leftBtn;
-    private Button rightBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_circle);
-        leftBtn = (Button) findViewById(R.id.left);
-        rightBtn = (Button) findViewById(R.id.right);
-        GameView gameView = new GameView(CircleActivity.this);
-        // EDGY CODE BELOW
-        //Bitmap bitmap = new Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
+        setContentView(new GameView(CircleActivity.this));
     }
 }
--- CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/GameView.java
+++ CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/GameView.java
@@ -1,18 +1,45 @@
 package com.tejas_bharadwaj.circlepong;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GameView extends View {
 
     private Paint paint;
+    private Drawable leftArrow = ContextCompat.getDrawable(getContext(), R.drawable
+            .ic_arrow_back_black_24dp);
+    private Drawable rightArrow = ContextCompat.getDrawable(getContext(), R.drawable
+            .ic_arrow_forward_black_24dp);
+    private Bitmap bitmapLeftArrow;
+    private Bitmap bitmapRightArrow;
 
     public GameView(Context context) {
         super(context);
         paint = new Paint();
+        bitmapLeftArrow = drawableToBitmap(leftArrow);
+        bitmapRightArrow = drawableToBitmap(rightArrow);
+    }
+
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        else {
+            Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable
+                    .getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        }
     }
 
     @Override
@@ -26,5 +53,22 @@ public class GameView extends View {
         canvas.drawPaint(paint);
         paint.setColor(Color.parseColor("#ff0000"));
         canvas.drawCircle(width / 2, height / 2, radius, paint);
+        canvas.drawBitmap(bitmapLeftArrow, width / 2 - 400, 1600, paint);
+        canvas.drawBitmap(bitmapRightArrow, width / 2 + 400, 1600, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                //if ()
+                return true;
+            }
+            default: {
+                return false;
+            }
+        }
     }
 }
