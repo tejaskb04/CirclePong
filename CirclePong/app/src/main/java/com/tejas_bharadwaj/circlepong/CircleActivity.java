cdc549132894438c59390a77b447fee50759b3fb
tejaskb04 <tejaskb@gmail.com>
2017-09-16 23:46:49 -0700

    Setup

--- CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/CircleActivity.java
+++ CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/CircleActivity.java
@@ -1,12 +1,12 @@
 package com.tejas_bharadwaj.circlepong;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class CircleActivity extends AppCompatActivity {
 
-    private GameView gameView;
     private Button leftBtn;
     private Button rightBtn;
 
@@ -14,8 +14,10 @@ public class CircleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle);
-        gameView = (GameView) findViewById(R.id.game_view);
         leftBtn = (Button) findViewById(R.id.left);
         rightBtn = (Button) findViewById(R.id.right);
+        GameView gameView = new GameView(CircleActivity.this);
+        // EDGY CODE BELOW
+        //Bitmap bitmap = new Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
     }
 }
